c5fd2a260291ce9981402d5058cf362c498d17bb
Thakur, Abhishek <Abhishek.Thakur@garmin.com>
2025-08-12 14:10:51 -0500

    Add ability to retrieve the product part number of the paired devices.
    Added a new parameter to the IQDevice class to retrieve the product part number of the paired devices. This will be populated only when using the Connect app with version 5.15 or later.
    Removed the outdated copy Comm Watch project and CommWatch.prg. Updated the README to point to the most up-to-date version of the same.

---
+++ Comm Android/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/Device.kt
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2025 Garmin International Ltd.
+ * Subject to Garmin SDK License Agreement and Wearables Application Developer Agreement.
+ */
+package com.garmin.android.apps.connectiq.sample.comm
+
+import com.garmin.android.connectiq.IQDevice
+
+class Device(val iqDevice: IQDevice, val partNumber: String? = null) {
+}
--- Comm Android/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/adapter/IQDeviceAdapter.kt
+++ Comm Android/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/adapter/IQDeviceAdapter.kt
@@ -11,12 +11,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.garmin.android.connectiq.IQDevice
+import com.garmin.android.apps.connectiq.sample.comm.Device
 import com.garmin.android.connectiq.IQDevice.IQDeviceStatus
 
 class IQDeviceAdapter(
-    private val onItemClickListener: (IQDevice) -> Unit
-) : ListAdapter<IQDevice, IQDeviceViewHolder>(IQDeviceItemDiffCallback()) {
+    private val onItemClickListener: (Device) -> Unit
+) : ListAdapter<Device, IQDeviceViewHolder>(IQDeviceItemDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IQDeviceViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -28,38 +28,42 @@ class IQDeviceAdapter(
         holder.bindTo(getItem(position))
     }
 
-    fun updateDeviceStatus(device: IQDevice, status: IQDeviceStatus?) {
+    fun updateDeviceStatus(device: Device, status: IQDeviceStatus?) {
         val index = currentList
-            .indexOfFirst { it.deviceIdentifier == device.deviceIdentifier }
-        currentList[index].status = status
+            .indexOfFirst { it.iqDevice.deviceIdentifier == device.iqDevice.deviceIdentifier }
+        currentList[index].iqDevice.status = status
         notifyItemChanged(index)
     }
 }
 
-private class IQDeviceItemDiffCallback : DiffUtil.ItemCallback<IQDevice>() {
-    override fun areItemsTheSame(oldItem: IQDevice, newItem: IQDevice): Boolean =
-        oldItem.deviceIdentifier == newItem.deviceIdentifier
+private class IQDeviceItemDiffCallback : DiffUtil.ItemCallback<Device>() {
+    override fun areItemsTheSame(oldItem: Device, newItem: Device): Boolean =
+        oldItem.iqDevice.deviceIdentifier == newItem.iqDevice.deviceIdentifier
 
-    override fun areContentsTheSame(oldItem: IQDevice, newItem: IQDevice): Boolean =
-        oldItem.deviceIdentifier == newItem.deviceIdentifier
-            && oldItem.friendlyName == newItem.friendlyName
-            && oldItem.status == newItem.status
+    override fun areContentsTheSame(oldItem: Device, newItem: Device): Boolean =
+        oldItem.iqDevice.deviceIdentifier == newItem.iqDevice.deviceIdentifier
+            && oldItem.iqDevice.friendlyName == newItem.iqDevice.friendlyName
+            && oldItem.iqDevice.status == newItem.iqDevice.status
 
 }
 
 class IQDeviceViewHolder(
     private val view: View,
-    private val onItemClickListener: (IQDevice) -> Unit
+    private val onItemClickListener: (Device) -> Unit
 ) : RecyclerView.ViewHolder(view) {
 
-    fun bindTo(device: IQDevice) {
-        val deviceName = when (device.friendlyName) {
-            null -> device.deviceIdentifier.toString()
-            else -> device.friendlyName
+    fun bindTo(device: Device) {
+        var deviceName = when (device.iqDevice.friendlyName) {
+            null -> device.iqDevice.deviceIdentifier.toString()
+            else -> device.iqDevice.friendlyName
+        }
+
+        if (!device.partNumber.isNullOrBlank()) {
+            deviceName += " (${device.partNumber})"
         }
 
         view.findViewById<TextView>(android.R.id.text1).text = deviceName
-        view.findViewById<TextView>(android.R.id.text2).text = device.status?.name
+        view.findViewById<TextView>(android.R.id.text2).text = device.iqDevice.status?.name
 
         view.setOnClickListener {
             onItemClickListener(device)
